fd9b8157e5fdd1a9acc4d1df9dafefdc552fe42c
Orson Peters <orsonpeters@gmail.com>
2022-12-06 14:07:52 +0100

    Simplified solution, 40% faster.

--- src/bin/day06_v2.rs
+++ src/bin/day06_v2.rs
@@ -4,11 +4,8 @@ fn find_disjoint_window(s: &[u8], n: usize) -> Option<usize> {
     let mut last_known_position = [0; 256];
     let mut start_disjoint = 0;
     for i in 0..s.len() {
-        let c = s[i] as usize;
-        if i - last_known_position[c] < n {
-            start_disjoint = start_disjoint.max(last_known_position[c] + 1);
-        }
-        last_known_position[c] = i;
+        start_disjoint = start_disjoint.max(last_known_position[s[i] as usize] + 1);
+        last_known_position[s[i] as usize] = i;
         if i >= start_disjoint + n {
             return Some(i);
         }
