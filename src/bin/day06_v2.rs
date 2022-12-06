b86997dd7ef5e303bf69aa0ec51c0e7fa025c551
Orson Peters <orsonpeters@gmail.com>
2022-12-06 13:25:52 +0100

    Added another day 6 solution.

---
+++ src/bin/day06_v2.rs
@@ -0,0 +1,30 @@
+use anyhow::{Context, Ok, Result};
+
+fn find_disjoint_window(s: &[u8], n: usize) -> Option<usize> {
+    let mut last_known_position = [0; 256];
+    let mut start_disjoint = 0;
+    for i in 0..s.len() {
+        let c = s[i] as usize;
+        if i - last_known_position[c] < n {
+            start_disjoint = start_disjoint.max(last_known_position[c] + 1);
+        }
+        last_known_position[c] = i;
+        if i >= start_disjoint + n {
+            return Some(i);
+        }
+    }
+    None
+}
+
+fn main() -> Result<()> {
+    let input = std::fs::read_to_string("inputs/day06.txt")?;
+    let start = std::time::Instant::now();
+
+    let bytes = input.trim().as_bytes();
+    let p1 = find_disjoint_window(bytes, 4);
+    let p2 = find_disjoint_window(bytes, 14);
+    println!("part1: {}", p1.context("marker not found")?);
+    println!("part2: {}", p2.context("marker not found")?);
+    println!("time: {:?}", start.elapsed());
+    Ok(())
+}
