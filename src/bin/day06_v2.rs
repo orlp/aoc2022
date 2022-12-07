febc9d4fc748243390e04fa8fbfec01ad622a8e0
Orson Peters <orsonpeters@gmail.com>
2022-12-07 13:37:51 +0100

    Fixed boundary condition bugs for day 6 v2.

--- src/bin/day06_v2.rs
+++ src/bin/day06_v2.rs
@@ -1,12 +1,12 @@
 use anyhow::{Context, Ok, Result};
 
 fn find_disjoint_window(s: &[u8], w: usize) -> Option<usize> {
-    let mut last_known_position = [0; 256];
+    let mut last_known_position = [usize::MAX; 256];
     let mut start_disjoint = 0;
     for i in 0..s.len() {
-        start_disjoint = start_disjoint.max(last_known_position[s[i] as usize] + 1);
+        start_disjoint = start_disjoint.max(last_known_position[s[i] as usize].wrapping_add(1));
         last_known_position[s[i] as usize] = i;
-        if i >= start_disjoint + w {
+        if i >= start_disjoint + w - 1 {
             return Some(i);
         }
     }
@@ -16,7 +16,7 @@ fn find_disjoint_window(s: &[u8], w: usize) -> Option<usize> {
 fn main() -> Result<()> {
     let input = std::fs::read_to_string("inputs/day06.txt")?;
     let start = std::time::Instant::now();
-    
+
     let p1 = find_disjoint_window(input.trim().as_bytes(), 4);
     let p2 = find_disjoint_window(input.trim().as_bytes(), 14);
 
