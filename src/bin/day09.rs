f9573548bad2b7f3486276d953bc5cc9373bb475
Orson Peters <orsonpeters@gmail.com>
2022-12-09 12:32:44 +0100

    Solved day 9.

---
+++ src/bin/day09.rs
@@ -0,0 +1,41 @@
+use std::collections::HashSet;
+
+use anyhow::{Context, Result};
+
+fn main() -> Result<()> {
+    let input = std::fs::read_to_string("inputs/day09.txt")?;
+    let start = std::time::Instant::now();
+
+    let mut rope = [[0i64; 2]; 10];
+    let mut part1_visited = HashSet::new();
+    let mut part2_visited = HashSet::new();
+    for line in input.lines() {
+        let (dir, n) = line.split_once(' ').context("bad input")?;
+        let delta = match dir.trim() {
+            "U" => [0, 1],
+            "D" => [0, -1],
+            "L" => [-1, 0],
+            "R" => [1, 0],
+            _ => anyhow::bail!("unknown direction"),
+        };
+
+        for _ in 0..n.parse()? {
+            rope[0] = std::array::from_fn(|i| rope[0][i] + delta[i]);
+            for t in 1..10 {
+                let (head, tail) = (rope[t - 1], &mut rope[t]);
+                if (0..2).any(|axis| head[axis].abs_diff(tail[axis]) > 1) {
+                    *tail = std::array::from_fn(|i| tail[i] + (head[i] - tail[i]).signum());
+                } else {
+                    break;
+                }
+            }
+            part1_visited.insert(rope[1]);
+            part2_visited.insert(rope[9]);
+        }
+    }
+
+    println!("part1: {}", part1_visited.len());
+    println!("part2: {}", part2_visited.len());
+    println!("time: {:?}", start.elapsed());
+    Ok(())
+}
