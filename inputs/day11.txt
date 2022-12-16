2fbecc0e1e5aae9b390573f14367707247a67190
Orson Peters <orsonpeters@gmail.com>
2022-12-16 14:18:14 +0100

    Solved day 11.

---
+++ inputs/day11.txt
@@ -0,0 +1,55 @@
+Monkey 0:
+  Starting items: 85, 77, 77
+  Operation: new = old * 7
+  Test: divisible by 19
+    If true: throw to monkey 6
+    If false: throw to monkey 7
+
+Monkey 1:
+  Starting items: 80, 99
+  Operation: new = old * 11
+  Test: divisible by 3
+    If true: throw to monkey 3
+    If false: throw to monkey 5
+
+Monkey 2:
+  Starting items: 74, 60, 74, 63, 86, 92, 80
+  Operation: new = old + 8
+  Test: divisible by 13
+    If true: throw to monkey 0
+    If false: throw to monkey 6
+
+Monkey 3:
+  Starting items: 71, 58, 93, 65, 80, 68, 54, 71
+  Operation: new = old + 7
+  Test: divisible by 7
+    If true: throw to monkey 2
+    If false: throw to monkey 4
+
+Monkey 4:
+  Starting items: 97, 56, 79, 65, 58
+  Operation: new = old + 5
+  Test: divisible by 5
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 5:
+  Starting items: 77
+  Operation: new = old + 4
+  Test: divisible by 11
+    If true: throw to monkey 4
+    If false: throw to monkey 3
+
+Monkey 6:
+  Starting items: 99, 90, 84, 50
+  Operation: new = old * old
+  Test: divisible by 17
+    If true: throw to monkey 7
+    If false: throw to monkey 1
+
+Monkey 7:
+  Starting items: 50, 66, 61, 92, 64, 78
+  Operation: new = old + 3
+  Test: divisible by 2
+    If true: throw to monkey 5
+    If false: throw to monkey 1
---
+++ src/bin/day11.rs
@@ -0,0 +1,88 @@
+use anyhow::{Ok, Result};
+use aoc2022::RegexExtract;
+use itertools::Itertools;
+use regex::Regex;
+use std::collections::VecDeque;
+
+const MONKEY_FORMAT: &'static str = r"Monkey \d+:
+\s*Starting items: (\d+(?:, \d+)*)
+\s*Operation: new = old ([*+]) (old|\d+)
+\s*Test: divisible by (\d+)
+\s*If true: throw to monkey (\d+)
+\s*If false: throw to monkey (\d+)";
+
+#[derive(Clone)]
+enum Operation {
+    AddConst(u64),
+    MulConst(u64),
+    Double,
+    Square,
+}
+
+#[derive(Clone)]
+struct Monkey {
+    items: VecDeque<u64>,
+    op: Operation,
+    divisor: u64,
+    targets: [usize; 2],
+    num_inspections: u64,
+}
+
+fn monkey_business(mut monkeys: Vec<Monkey>, rounds: usize, div: u64, rem: u64) -> u64 {
+    for _round in 0..rounds {
+        for m in 0..monkeys.len() {
+            while let Some(item) = monkeys[m].items.pop_front() {
+                let afterop = match monkeys[m].op {
+                    Operation::AddConst(c) => item + c,
+                    Operation::MulConst(c) => item * c,
+                    Operation::Double => item + item,
+                    Operation::Square => item * item,
+                };
+                let new = afterop / div % rem;
+                let target = monkeys[m].targets[(new % monkeys[m].divisor == 0) as usize];
+                monkeys[target].items.push_back(new);
+                monkeys[m].num_inspections += 1;
+            }
+        }
+    }
+
+    monkeys.select_nth_unstable_by(1, |a, b| b.num_inspections.cmp(&a.num_inspections));
+    monkeys[0].num_inspections * monkeys[1].num_inspections
+}
+
+fn main() -> Result<()> {
+    let input = std::fs::read_to_string("inputs/day11.txt")?;
+    let start = std::time::Instant::now();
+
+    let re = Regex::new(MONKEY_FORMAT)?;
+    let monkeys: Vec<Monkey> = re
+        .extract_iter(&input)
+        .map(|(_, note)| {
+            let [start, optype, oparg, div, iftrue, iffalse] = note;
+            let op = match optype {
+                "+" if oparg == "old" => Operation::Double,
+                "+" => Operation::AddConst(oparg.parse()?),
+                "*" if oparg == "old" => Operation::Square,
+                "*" => Operation::MulConst(oparg.parse()?),
+                _ => unreachable!(),
+            };
+
+            Ok(Monkey {
+                items: start.split(',').map(|s| s.trim().parse()).try_collect()?,
+                op,
+                divisor: div.parse()?,
+                targets: [iffalse.parse()?, iftrue.parse()?],
+                num_inspections: 0,
+            })
+        })
+        .try_collect()?;
+
+    let rem = monkeys.iter().map(|m| m.divisor).product();
+    let part1 = monkey_business(monkeys.clone(), 20, 3, 1u64 << 63);
+    let part2 = monkey_business(monkeys, 10000, 1, rem);
+
+    println!("part1: {part1}");
+    println!("part2: {part2}");
+    println!("time: {:?}", start.elapsed());
+    Ok(())
+}
